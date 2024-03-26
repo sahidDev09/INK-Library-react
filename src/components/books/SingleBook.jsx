8f68f65c59470991f08a074ef795f2d1f78534bb
MD SAHID AHMED <iamsahidofficial99@gmail.com>
2024-03-26 12:13:37 +0600

    reorganize code and add some hover effects on card

--- src/components/books/SingleBook.jsx
+++ src/components/books/SingleBook.jsx
@@ -5,13 +5,21 @@ const SingleBook = ({ books }) => {
   const { bookname, author, image, tags, category, rating } = books;
 
   return (
-    <div className=" border rounded-xl p-5 flex flex-col gap-4">
-      <div className=" bg-slate-200 rounded-xl h-[250px] p-10">
-      <img className=" m-auto w-28 object-cover items-center h-full" src={image} alt="" />
+    <div className=" border rounded-xl p-5 flex flex-col gap-4 hover:border hover:border-amber-600 transition-all">
+      <div className=" bg-slate-200 rounded-xl h-[250px] p-10 drop-shadow-md">
+        <img
+          className="m-auto w-28 object-cover items-center h-full"
+          src={image}
+          alt=""
+        />
       </div>
       <div className=" flex gap-2">
         {tags.map((tag, index) => (
-          <button className="p-2 px-4 text-green-700 bg-green-100 rounded-full" key={index}>{tag}</button>
+          <button
+            className="p-2 px-4 text-green-700 bg-green-100 rounded-full"
+            key={index}>
+            {tag}
+          </button>
         ))}
       </div>
       <h1 className=" text-xl font-bold">{bookname}</h1>
